5277b5a27ae95260b0d3c2fa49eb8716fe93f715
konstantin <serebryakov.kosyua@gmail.com>
2023-05-13 16:51:01 +0300

    final

--- bot/bot_main.py
+++ bot/bot_main.py
@@ -2,12 +2,10 @@ import os
 
 from telegram import Update
 from telegram.ext import Application, ContextTypes, CommandHandler
-from sqlalchemy.orm import sessionmaker
 from bot import bot_messages
 from db.db_api import Database
 from bot.stats import generate_stats_by_month
 
-from db.models import User
 
 history = ['Income 25.03.2023 100', 'Income 26.03.2023 200', 'Expense 27.03.2023 150']
 
@@ -33,10 +31,11 @@ class Bot:
         if len(user_data) == 4:
             if not self.db.is_user_exists(user_data[1], user_data[0], user_data[2]):
                 self.db.save_user(user_data=user_data)
-                self.logged = True
+                self.login_card_number = -1
                 await update.message.reply_text(bot_messages.set_service_success_msg(user_data[1]))
             else:
-                await context.bot.send_message(chat_id=update.effective_chat.id, text=bot_messages.ALREADY_EXISTS_USER_MSG)
+                await context.bot.send_message(chat_id=update.effective_chat.id,
+                                               text=bot_messages.ALREADY_EXISTS_USER_MSG)
         else:
             await context.bot.send_message(chat_id=update.effective_chat.id, text=bot_messages.INCORRECT_INPUT_MSG)
 
@@ -44,7 +43,8 @@ class Bot:
         user_data = context.args
         if len(user_data) == 2:
             if self.login_card_number != -1:
-                await context.bot.send_message(chat_id=update.effective_chat.id, text=bot_messages.ALREADY_LOGIN_USER_MSG)
+                await context.bot.send_message(chat_id=update.effective_chat.id,
+                                               text=bot_messages.ALREADY_LOGIN_USER_MSG)
             # await context.bot.send_message(chat_id=update.effective_chat.id, text=bot_messages.SUCCESS_LOGIN_MSG)
             elif self.db.login_user(user_data[1], int(user_data[0])):
                 self.login_card_number = int(user_data[0])
@@ -71,19 +71,16 @@ class Bot:
         else:
             await context.bot.send_message(chat_id=update.effective_chat.id, text=bot_messages.TRANSFER_INPUT_ERROR_MSG)
 
-
-    async def balance(self, update : Update, context : ContextTypes.DEFAULT_TYPE):
-        if self.logged:
+    async def balance(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
+        if self.login_card_number != -1:
             await context.bot.send_message(chat_id=update.effective_chat.id, text="On your account X rubles")
         else:
             await context.bot.send_message(chat_id=update.effective_chat.id, text=bot_messages.NOT_AUTHORIZED)
-            
 
-    async def history(self, update : Update, context : ContextTypes.DEFAULT_TYPE):
+    async def history(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
         await context.bot.send_message(chat_id=update.effective_chat.id, text=bot_messages.get_history(history=history))
 
-
-    async def stats(self, update : Update, context : ContextTypes.DEFAULT_TYPE):
+    async def stats(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
         await context.bot.send_message(chat_id=update.effective_chat.id, text='график.png')
         user_data = context.args
         if len(user_data) == 1:
@@ -92,8 +89,7 @@ class Bot:
             os.remove(path='график.png')
         else:
             pass
-            
+
     @staticmethod
     async def start(update: Update, _: ContextTypes.DEFAULT_TYPE):
         await update.message.reply_text(bot_messages.HELLO_MSG)
-
